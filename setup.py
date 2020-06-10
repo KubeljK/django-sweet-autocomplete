a2a8fe1166365127e23b81b7110cd97944ac715a
klemen <klemen.kubelj@gmail.com>
2020-06-10 08:50:33 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 setup(
     name='django-sweet-autocomplete',
-    version='0.0.4',
+    version='1.0.0',
     package_dir={'': 'src'},
     packages=['sweetautocomplete'],
     license='MIT',
